a38b07dc23ff9c6b3eb47269ce68ad57b5749f47
deevin3 <oviedodivine@gmail.com>
2023-02-02 02:33:21 -0800

    first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-/**
- *
- *  *_strcat - concatenates two strings
+
+ 
+ *  /**  *_strcat - concatenates two strings
  *
  *   *@dest: A pointer to a character that will be changed
  *
  *    *@src: A pointer to a character that will also be changed
  *
- *     *Return: dest */
+ *     *Return: dest */ 
  *
-
+ *      
 
 
 char *_strcat(char *dest, char *src)
